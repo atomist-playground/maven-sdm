7631044c7beb0eb638b83d9e8213d4b6389cfe2c
Christian Dupuis <cd@atomist.com>
2019-11-04 23:12:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sdm_core_1 = require("@atomist/sdm-core");
-var os = require("os");
-var path = require("path");
+const sdm_core = require("@atomist/sdm-core");
+const os = require("os");
+const path = require("path");
+
 exports.cfg = {
     name: "@atomist/demo-sdm",
-    logging: {
-        callsite: true,
-    },
     sdm: {
         cache: {
             enabled: true,
             path: path.join(os.homedir(), ".atomist", "cache", "container"),
-            store: new sdm_core_1.CompressingGoalCache(),
+            store: new sdm_core.CompressingGoalCache(),
         },
     },
 };
