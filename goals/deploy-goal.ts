3862bac8f1420c04914a22d8ff86afc6bc69c506
Christian Dupuis <cd@atomist.com>
2019-11-04 21:05:48 +0100

    Fix

--- goals/deploy-goal.ts
+++ goals/deploy-goal.ts
@@ -8,13 +8,14 @@ import { codeLine } from "@atomist/slack-messages";
 export const deploy: GoalMaker = () => goal(
     { displayName: "Deploy" },
     async gi => {
-        gi.progressLog.wr
+        gi.progressLog.write("atm:phase=deploying");
         await gi.addressChannels(
             slackSuccessMessage(
                 "Deployment",
                 `Deployed Docker image\n${codeLine((gi as any).parameters.image)}`,
             ),
         );
+        gi.progressLog.write("atm:phase=deployed");
     },
     {
         plan: async pli => ({
