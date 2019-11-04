dc530f3e359d530852082d95b61025ca066f1da4
Christian Dupuis <cd@atomist.com>
2019-11-04 22:16:36 +0100

    Format

--- goals/deploy-goal.js
+++ goals/deploy-goal.js
@@ -3,7 +3,9 @@ const slack_messages = require("@atomist/slack-messages");
 
 exports.deploy = () => sdm.goal({ displayName: "Deploy" }, async (gi) => {
     gi.progressLog.write("atm:phase=deploying");
-    await gi.addressChannels(sdm.slackSuccessMessage("Deployment", `Deployed Docker image\n${slack_messages.codeLine(gi.parameters.image)}`));
+    await gi.addressChannels(sdm.slackSuccessMessage(
+        "Deployment",
+        `Deployed Docker image\n${slack_messages.codeLine(gi.parameters.image)}`));
     gi.progressLog.write("atm:phase=deployed");
 }, {
     plan: async (pli) => ({
