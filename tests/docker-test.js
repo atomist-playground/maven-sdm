af38bb87448452c702ac7314b9e17a4819f0be9a
Christian Dupuis <cd@atomist.com>
2019-11-05 16:27:17 +0100

    Fix goalTest to use new image location

--- tests/docker-test.js
+++ tests/docker-test.js
@@ -4,7 +4,9 @@ const _ = require("lodash");
 exports.HasImage = () => sdm.goalTest(
     "Has Docker image",
     async (g, pli) => {
-        if (g.name === "docker-build" && g.state === sdm.SdmGoalState.success && !!_.get(g, "push.after.images[0].imageName")) {
+        if (g.name === "docker-build" &&
+            g.state === sdm.SdmGoalState.success &&
+            !!_.get(g, "push.after.images[0].imageName")) {
             const image = _.get(g, "push.after.images[0].imageName");
             if (!!image) {
                 pli.facts = {
