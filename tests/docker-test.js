349945d9f2b6f8f15c70e502c5652d1245e50bc7
Christian Dupuis <cd@atomist.com>
2019-11-05 16:11:50 +0100

    Update goalTest

--- tests/docker-test.js
+++ tests/docker-test.js
@@ -1,10 +1,11 @@
 const sdm = require("@atomist/sdm");
+const _ = require("lodash");
 
 exports.HasImage = () => sdm.goalTest(
     "Has Docker image",
     async (g, pli) => {
-        if (g.state === sdm.SdmGoalState.success && !!g.data) {
-            const image = JSON.parse(g.data).image;
+        if (g.name === "docker-build" && g.state === sdm.SdmGoalState.success && !!_.get(g, "push.after.images[0].imageName")) {
+            const image = _.get(g, "push.after.images[0].imageName");
             if (!!image) {
                 pli.facts = {
                     image,
