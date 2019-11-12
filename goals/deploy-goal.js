fd916ec8642d5cc1a6e7aed1e82feb57db51720b
Christian Dupuis <cd@atomist.com>
2019-11-12 10:52:41 +0100

    Add goal parametes

--- goals/deploy-goal.js
+++ goals/deploy-goal.js
@@ -1,12 +1,12 @@
 const sdm = require("@atomist/sdm");
 const slack_messages = require("@atomist/slack-messages");
 
-exports.deploy = () => sdm.goal(
+exports.deploy = (s, params) => sdm.goal(
     {displayName: "deploy"},
     async (gi) => {
         await gi.addressChannels(sdm.slackSuccessMessage(
             "Deployment",
-            `Deployed Docker image\n${slack_messages.codeLine(gi.parameters.image)}`));
+            `Deployed Docker image\n${slack_messages.codeLine(gi.parameters.image)}\nto environment ${slack_messages.italic(params.environment)}`));
     },
     {
         plan: async (pli) => ({
